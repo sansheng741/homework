a4d98464b85188ac99064dda120792e37c15e428
sansheng741 <807156237@qq.com>
2020-08-27 22:56:11 +0800

    [clean code] [colin] test : add test should return 3 when deliveryDate given Rush is true and deliveryState is NY

--- test/deliveryDateTest.js
+++ test/deliveryDateTest.js
@@ -8,4 +8,12 @@ test('should return 2 when deliveryDate given Rush is true and deliveryState is 
   t.is(result, 2);
 });
 
+test('should return 3 when deliveryDate given Rush is true and deliveryState is NY', t => {
+  let anOrder = {deliveryState : 'NY', placedOn : {plusDays : (x) => {return x}}}
+  let isRush = true;
+  const result = deliveryDate(anOrder, isRush);
+  t.is(result, 3);
+});
+
+
 
