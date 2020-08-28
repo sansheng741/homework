3a1ce621561202e1d37f1baf31c1513422be53b9
sansheng741 <807156237@qq.com>
2020-08-28 11:58:19 +0800

    [clean code] [colin] test : add test should return 3 when voyageRisk given voyage.zone null and voyage.length 5

--- test/rankTest.js
+++ test/rankTest.js
@@ -1,5 +1,6 @@
 const rankTest = require('ava');
 const {voyageRisk} = require('../src/rank');
+
 rankTest('foo', t => {
   t.pass();
 });
@@ -16,4 +17,13 @@ rankTest('should return 1 when voyageRisk given voyage.zone null and voyage.leng
   };
   const result = voyageRisk(voyage);
   t.is(result,1);
+});
+
+rankTest('should return 3 when voyageRisk given voyage.zone null and voyage.length 5', t => {
+  const voyage = {
+    zone: '',
+    length: 5,
+  };
+  const result = voyageRisk(voyage);
+  t.is(result,3);
 });
